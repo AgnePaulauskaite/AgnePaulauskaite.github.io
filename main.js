865ff2cb08d90e9871c67491831f3c0ac444e44f
AgnePaulauskaite <agnepaulauskaite@economist.com>
2019-05-28 17:39:18 +0100

    correct js functin

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-(function() {
+(function () {
     'use strict'
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
   
-    var c = shape.Circle(200, 200, 200);
+    var c = Shape.Circle(200, 200, 200);
     c.fillColor = 'green';
 
     paper.view.draw();
 
-} ())
+}())
+
+
