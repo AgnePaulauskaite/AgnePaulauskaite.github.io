03c6ae2300d3de50e5703f803887409ac320a7aa
AgnePaulauskaite <agnepaulauskaite@economist.com>
2019-05-29 20:37:46 +0100

    automating repetitive tasks

--- main.js
+++ main.js
@@ -3,9 +3,13 @@
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
   
-    var c = Shape.Circle(200, 200, 200);
-    c.fillColor = 'green';
-
+    var c; //used for loop
+    for(var x=25; x<400; x+=50) {
+        for(var y=25; y<400; y+=50) {
+            c = Shape.Circle(x, y, 20);
+            c.fillColor = 'red';
+        }
+    } 
     paper.view.draw();
 
 }())
