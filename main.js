f6f912e0db47a639127fbfb430c8e45bc5106386
AgnePaulauskaite <agnepaulauskaite@economist.com>
2019-05-29 21:29:09 +0100

    user input but needs to correct it

--- main.js
+++ main.js
@@ -3,13 +3,21 @@
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
   
-    var c; //used for loop
-    for(var x=25; x<400; x+=50) {
-        for(var y=25; y<400; y+=50) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = 'red';
-        }
-    } 
+    var tool = new tool();
+    
+    var c = Shape.Circle(200, 200, 8);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello word';
+
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+        c.fillColor = 'blue';
+
+    };
     paper.view.draw();
 
 }())
